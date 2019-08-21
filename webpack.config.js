7f87295143efe8c2226088da2974525b10d8f568
haitao <weihaitao@huohetech.com>
2019-08-21 20:57:05 +0800

    Ver 1.2 管理输出 - 清理dist、 配置html模板和压缩方式

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// 返回对象
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -7,8 +9,19 @@ module.exports = {
     print: './src/print.js'
   },
   plugins: [
+    new CleanWebpackPlugin(), // 自动清理output指定的目录
     new HtmlWebpackPlugin({
-      title: 'Output Management'
+      title: 'Output Management',
+      template: 'index.html',
+      inject: 'body',
+      minify: { // 压缩
+        removeComments: true, // 去除注释
+        collapseWhitespace: true, // 去掉空格
+        removeAttributeQuotes: true, // 去掉属性引号
+        minifyCSS: true // 压缩css
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      }
     })
   ],
   output: {
