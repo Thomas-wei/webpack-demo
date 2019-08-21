384a71baf41f87355befb1a5fc1c7224a4655e3c
haitao <weihaitao@huohetech.com>
2019-08-21 17:37:25 +0800

    ver 1.1 加载各种文件

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import _ from 'lodash';
+import './style.css';
+import './iconfont/iconfont.css';
+import Icon from './expand.png';
+import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
   // lodash. now imported by this script
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  element.classList.add('iconfont');
+  element.classList.add('icon-shanchutupian');
 
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
   return element;
 }
 
