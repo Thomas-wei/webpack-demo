ce1ab2ec9f48f8078091d610d399034824ec5148
haitao <weihaitao@huohetech.com>
2019-08-21 14:53:16 +0800

    ver 1.0 第一次bundle打包

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import _ from 'lodash';
+
 function component() {
   var element = document.createElement('div');
 
+  // lodash. now imported by this script
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
 
   return element;
