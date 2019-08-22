9528e5fcd048a64c4f4a672ef1133857db0a5195
haitao <weihaitao@huohetech.com>
2019-08-22 15:00:44 +0800

    Ver 1.4 模块热替换 - Node api 进行热替换

--- src/print.js
+++ src/print.js
@@ -1,4 +1,4 @@
 export default function printMe() {
   console.log('I get called from print.js1111!')
-  console.log('1111111111111111111')
+  console.log('333333333')
 }
--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,6 @@ module.exports = {
     app: './src/index.js'
   },
   devtool: "cheap-source-map",
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
   plugins: [
     new CleanWebpackPlugin(), // 自动清理output指定的目录
     new HtmlWebpackPlugin({
